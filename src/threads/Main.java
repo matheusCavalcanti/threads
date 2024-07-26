1c6df469221df69bd997a919b8f6bbf5854ce3e4
Matheus <vte_msantos@uolinc.com>
2024-07-26 12:45:53 -0300

    Aula 1

---
+++ src/threads/Main.java
@@ -0,0 +1,9 @@
+package threads;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Isso é uma thread main");
+
+        Thread.sleep(50000);
+    }
+}
